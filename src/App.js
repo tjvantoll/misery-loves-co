9733be05d6125d4115dd95733af1809c56866929
TJ VanToll <tj.vantoll@gmail.com>
2021-02-12 12:18:14 -0500

    Music and stuff

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   const [allArrangements, setAllArrangements] = React.useState([]);
-  const [weapons, setWeapons] = React.useState([]);
   const [items, setItems] = React.useState([]);
+  const [cacti, setCacti] = React.useState([]);
+  const [touchMeNots, setTouchMeNots] = React.useState([]);
+  const [weapons, setWeapons] = React.useState([]);
+
+  const audioTag = React.useRef(null);
+
+  const [showFlowers, setShowFlowers] = React.useState(true);
   const [horrorMode, setHorrorMode] = React.useState(false);
+  const [myersMode, setMyersMode] = React.useState(false);
+
   const [isDisabled, setDisabled] = React.useState(false);
 
   React.useEffect(() => {
@@ -14,6 +22,22 @@ function App() {
       .then(data => {
         setAllArrangements(data);
         setItems(data);
+      })
+  }, []);
+
+  React.useEffect(() => {
+    fetch('https://my-json-server.typicode.com/samidip/valentinegrinch/cactuses')
+      .then(response => response.json())
+      .then(data => {
+        setCacti(data);
+      })
+  }, []);
+
+  React.useEffect(() => {
+    fetch('https://my-json-server.typicode.com/samidip/valentinegrinch/touchmenots')
+      .then(response => response.json())
+      .then(data => {
+        setTouchMeNots(data);
       })
   }, []);
 
@@ -32,13 +56,39 @@ function App() {
       setItems([...weapons]);
       setDisabled(true);
       setHorrorMode(true);
+      setShowFlowers(false);
+      setMyersMode(false);
       setTimeout(() => {
         setDisabled(false);
       }, 3000);
       return;
     }
 
+    if (value.toLowerCase().includes('ros')) {
+      setMyersMode(true);
+      setShowFlowers(true);
+      audioTag.current.currentTime = 0;
+      audioTag.current.play();
+      return;
+    }
+
+    if (value.toLowerCase().includes('ro')) {
+      setMyersMode(false);
+      audioTag.current.pause();
+      setHorrorMode(false);
+      setItems([...touchMeNots]);
+      return;
+    }
+
+    if (value.toLowerCase().includes('r')) {
+      setItems([...cacti]);
+      return;
+    }
+
     setHorrorMode(false);
+    setMyersMode(false);
+    audioTag.current.pause();
+    setShowFlowers(true);
     const newArrangements = allArrangements.filter(arrangement => {
       if (value === '') {
         return true;
@@ -51,13 +101,20 @@ function App() {
 
   return (
     <>
+      <div className={myersMode ? 'myers-mode' : ''}>
       <div className={horrorMode ? 'horror-mode' : ''}>
         <header className="header">
-          <h1>Misery Loves Co. 🔪🩸</h1>
+          <h1>
+            { horrorMode ? 'Misery Loves Co.' : 'Misery Flowers' }
+            <span onMouseOver={() => { setShowFlowers(!showFlowers)}} onMouseOut={() => { setShowFlowers(!showFlowers)}}>
+              <span style={{ display: showFlowers ? 'inline' : 'none' }}>🌷🌻</span>
+              <span style={{ display: showFlowers ? 'none' : 'inline' }}>🔪🩸</span>
+            </span>
+          </h1>
         </header>
         <form>
-          <label htmlFor="Search">Search:</label>
-          <input disabled={isDisabled ? 'disabled' : ''} id="Search" type="text" name="flower" onChange={filter} />
+          <label className="sr-only" htmlFor="Search">Search:</label>
+          <input placeholder="Pick your poison...roses?" disabled={isDisabled ? 'disabled' : ''} id="Search" type="text" name="flower" onChange={filter} />
         </form>
         <div className="card-list">
           {items.map(item => (
@@ -67,6 +124,10 @@ function App() {
             </div>
           ))}
         </div>
+
+        <audio ref={audioTag} src="/halloween-theme.mp3"></audio>
+        <img className="myers" alt="Michael Myers says hi" src="/myers.png" />
+      </div>
       </div>
     </>
   );
